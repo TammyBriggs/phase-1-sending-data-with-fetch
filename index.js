b402c49ac01f2c971cfd7ef42d8837d353023292
TammyBriggs <tamunotonye.briggs@student.moringaschool.com>
2023-03-31 11:54:03 +0300

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+    const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+        name: "Tammy",
+        email: "tammybriggs@gmail.com",
+      }),
+  };
+  
+  function submitData() {
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+        const newId = object.id;
+        document.body.innerHTML += newId;
+    })
+    .catch(function (error) {
+        document.body.innerHTML += error.message;
+    })
+};
